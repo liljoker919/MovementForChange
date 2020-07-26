a2feb6677545663d97a1b9c9b890fc0a9a985b82
liljoker919 <chrislnickerson@outlook.com>
2020-07-26 15:00:33 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovementForChange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
